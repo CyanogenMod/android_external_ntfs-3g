57317e733c81246ebfe6b4465db3ebdd60efc765
Michael Bestas <mikeioannina@gmail.com>
2015-10-09 12:54:00 +0300

    ntfs-3g: Fix compilation issues on M
    Change-Id: I7b3dad57117112125ef64f7441796c300789addc

--- config.h
+++ config.h
@@ -45,6 +45,9 @@
 /* Define to 1 if you have the `dup2' function. */
 #define HAVE_DUP2 1
 
+/* Define to 1 if you have the <endian.h> header file. */
+#define HAVE_ENDIAN_H
+
 /* Define to 1 if you have the <errno.h> header file. */
 #define HAVE_ERRNO_H 1
 
